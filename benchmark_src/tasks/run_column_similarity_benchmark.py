771eab1c21a125d5c7deefe638a4384658ee8763
Niharika-DSouza <Niharika.Dsouza@ibm.com>
2025-10-29 10:50:43 -0700

    Fix column similarity search bug and contexttab ZMQ timeout

--- benchmark_src/tasks/run_column_similarity_benchmark.py
+++ benchmark_src/tasks/run_column_similarity_benchmark.py
@@ -175,6 +175,7 @@ def run_inference_based_on_column_embeddings(cluster_ranges, cfg):
             new_gt = gt_data
             for k in gt_data:
                 table = k.split('.')[0]
+                searched_indexes.append((table, k))
                 search_sources.append(all_columns[table][k])
 
         search_sources = np.asarray(search_sources)
