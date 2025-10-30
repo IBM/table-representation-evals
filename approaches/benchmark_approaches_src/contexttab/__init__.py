8b4c0ce14a2a2835baf8cc876d0aa69c6b257cd1
Niharika-DSouza <Niharika.Dsouza@ibm.com>
2025-10-30 10:16:19 -0700

    ContextTab: add column embeddings support and config

--- approaches/benchmark_approaches_src/contexttab/__init__.py
+++ approaches/benchmark_approaches_src/contexttab/__init__.py
@@ -1,4 +1,7 @@
 from .approach import ConTextTabEmbedder
 from .predictive_ml_component import PredictiveMLComponent
+from .column_embedding_component import ColumnEmbeddingComponent
 
-__all__ = ['ConTextTabEmbedder', 'PredictiveMLComponent']
+__all__ = ['ConTextTabEmbedder', 'PredictiveMLComponent', 'ColumnEmbeddingComponent']
+
+
---
+++ approaches/benchmark_approaches_src/contexttab/column_embedding_component.py
@@ -0,0 +1,25 @@
+import pandas as pd
+import numpy as np
+
+from benchmark_src.approach_interfaces.column_embedding_interface import ColumnEmbeddingInterface
+
+
+class ColumnEmbeddingComponent(ColumnEmbeddingInterface):
+    """
+    Column embedding component for ConTextTab approach.
+    Delegates to the ConTextTab embedder's get_column_embeddings.
+    """
+
+    def __init__(self, approach_instance):
+        self.approach_instance = approach_instance
+
+    def setup_model_for_task(self, input_table: pd.DataFrame, dataset_information: dict):
+        # Ensure model is loaded prior to embedding generation
+        self.approach_instance.load_trained_model()
+
+    def create_column_embeddings_for_table(self, input_table: pd.DataFrame):
+        column_embeddings, column_names = self.approach_instance.get_column_embeddings(input_table)
+        # Return as numpy array and names for downstream tasks
+        return column_embeddings, column_names
+
+
