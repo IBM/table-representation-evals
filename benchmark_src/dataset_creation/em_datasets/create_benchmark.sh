9729f840d088ddc47d8742e54da794d9e8e8a9eb
Liane Vogel <liane.vogel@cs.tu-darmstadt.de>
2025-11-19 14:56:51 +0000

    Moved code to create em_datasets to src folder

--- benchmark_src/dataset_creation/em_datasets/create_benchmark.sh
+++ benchmark_src/dataset_creation/em_datasets/create_benchmark.sh
@@ -3,11 +3,11 @@
 ############## adapt these parameters! ###################
 
 # where do you want the raw datasets to be downloaded/saved to?
-raw_datasets_dir="../raw_datasets"
+raw_datasets_dir="./cache/raw_datasets"
 
 # where do you want the created datasets be saved to?
 ## Foldername needs to be new to not override previously created datasets
-output_dir="./created_datasets/row_similarity_data_full"
+output_dir="./cache/row_similarity_data_full"
 
 # limit input tables to a maximum number of rows, set to null if you want to use all rows
 table_row_limit=null 
@@ -22,5 +22,5 @@ datasets=(
 ##########################################################
 
 for dataset in "${datasets[@]}"; do
-    python dataset_creation/dataset_creation_src/prepare_em_datasets.py output_dir=$output_dir dataset=$dataset raw_datasets_dir=$raw_datasets_dir table_row_limit=$table_row_limit
+    python ./benchmark_src/dataset_creation/em_datasets/dataset_creation_src/prepare_em_datasets.py output_dir=$output_dir dataset=$dataset raw_datasets_dir=$raw_datasets_dir table_row_limit=$table_row_limit
 done
