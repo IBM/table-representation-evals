bdf5a16273d4ccaf95e8ca109e1a66253c2c31f4
Liane Vogel <liane.vogel@cs.tu-darmstadt.de>
2026-01-06 13:47:17 +0000

    added nextia to create join benchmark script

--- benchmark_src/dataset_creation/create_join_benchmark.sh
+++ benchmark_src/dataset_creation/create_join_benchmark.sh
@@ -36,9 +36,61 @@ else
     rm -rf ./valentine
     mkdir ./valentine
     mv Valentine-datasets ./valentine
-    for f in ./valentine/Valentine-datasets/*/Semantically-Joinable/*/*mapping*.json;do `python convert_valentine_gt.py $f`;done
+    for f in ./valentine/*/Semantically-Joinable/*/*mapping*.json;do `python convert_valentine_gt.py $f`;done
     rm Valentine-datasets.zip
 fi
+
+
+# NextIA
+if [ -d "./nextia/testbedS/datalake" ] && [ "$(ls -A ./nextia/testbedS/datalake)" ]; then
+    echo "the NextIA dataset was already downloaded"
+else
+    wget -O testbedS.zip https://mydisk.cs.upc.edu/s/dX3FajwWZn7rrrd/download
+    unzip testbedS.zip
+    mv testbedS/datasets ./nextia/testbedS/datalake
+    rm -rf testbedS
+    rm testbedS.zip
+
+    wget -O testbedM.zip https://mydisk.cs.upc.edu/s/niPyR4WTtxydprj/download
+    unzip testbedM.zip
+    mv testbedM/datasets ./nextia/testbedM/datalake
+    rm -rf testbedM
+    rm testbedM.zip
+fi
+
+# Wiki Join
+if [ -d "./wikijoin/datalake" ] && [ "$(ls -A ./wikijoin/datalake)" ]; then
+    echo "the wikijoin dataset was already downloaded"
+else
+    echo "Downloading WikiJoin"
+    wget -O ./wikijoin/original.tar.bz2 https://zenodo.org/records/10042019/files/wiki-join-search.tar.bz2
+    echo "Setting up Wiki Join"
+    cd wikijoin
+    tar -xvzf gt.jsonl.tar.gz
+    mkdir ./datalake
+    tar -xjf ./original.tar.bz2
+    mv wiki-join-search/tables-with-headers/ datalake
+    rm -r wiki-join-search
+    rm -r ./original.tar.bz2
+    echo "WikiJoin Setup Done"
+    cd ..
+fi
+
+# Auto Join
+if [ -d "./autojoin/datalake" ] && [ "$(ls -A ./autojoin/datalake)" ]; then
+    echo "the autojoin dataset was already downloaded"
+else
+    echo "Dowloading AutoJoin"
+    rm -rf ./autojoin/original
+    rm -rf ./autojoin/datalake
+    git clone https://github.com/Yeye-He/Auto-Join.git ./autojoin/original
+    cd autojoin
+    mkdir -p datalake
+    sh ./create_datalake.sh
+    rm -rf ./autojoin/original
+    echo "AutoJoin Setup Done"
+fi
+
 
 popd
 
